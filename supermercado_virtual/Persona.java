207d37caeb805513f402694de4542c8a34b787cb
Edwin Jimenez Gomez <erjimene@unal.edu.co>
2020-09-30 09:31:07 -0500

    cam2

---
+++ supermercado_virtual/Persona.java
@@ -0,0 +1,12 @@
+package supermercado_virtual;
+
+public class Persona {
+	
+	private String nombre;
+	private String id;
+	private String genero;
+	private String direccion;
+	private int tel_fijo;
+	private int num_calular;
+
+}
