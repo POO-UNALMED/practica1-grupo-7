a7083fdc236ae92672d25de3a04bdbf487eb45a9
aaaahhhh4 <53578851+aaaahhhh4@users.noreply.github.com>
2020-10-04 22:12:37 -0500

    Commit 0.4

--- src/gestorAplicacion/productos/DetalleFactura.java
+++ src/gestorAplicacion/productos/DetalleFactura.java
@@ -1,5 +1,16 @@
 package gestorAplicacion.productos;
 
 public class DetalleFactura {
-
+	private Factura id_factura;
+	private Producto id_producto;
+	private int cantidad;
+	private double subtotal;
+	public DetalleFactura(Factura id_factura, Producto id_producto, int cantidad, double subtotal) {
+		super();
+		this.id_factura = id_factura;
+		this.id_producto = id_producto;
+		this.cantidad = cantidad;
+		this.subtotal = subtotal;
+	}
+	
 }
