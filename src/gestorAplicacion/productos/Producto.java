a708f81913ae0fb99ae4b5e8b0a2071ebb766e3a
Saherrerap <69688276+Saherrerap@users.noreply.github.com>
2020-10-04 22:23:51 -0500

    LPM 2.0

--- src/gestorAplicacion/productos/Producto.java
+++ src/gestorAplicacion/productos/Producto.java
@@ -1,5 +1,18 @@
 package gestorAplicacion.productos;
 
 public class Producto {
+	public String id_producto;
+	public int precio;
+	public String nom_producto;
+	public int contador;
+	public String tipo_producto;
+	public Producto(String id_producto, int precio, String nom_producto, int contador, String tipo_producto) {
+		this.id_producto = id_producto;
+		this.precio = precio;
+		this.nom_producto = nom_producto;
+		this.contador = contador;
+		this.tipo_producto = tipo_producto;
+	}
+	
 
 }
