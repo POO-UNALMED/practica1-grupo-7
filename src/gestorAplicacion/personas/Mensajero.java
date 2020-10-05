90115172b18c905e7c3717f48e2cab7ad7082e6a
Saherrerap <69688276+Saherrerap@users.noreply.github.com>
2020-10-04 22:16:22 -0500

    Update Mensajero.java

--- src/gestorAplicacion/personas/Mensajero.java
+++ src/gestorAplicacion/personas/Mensajero.java
@@ -1,5 +1,19 @@
 package gestorAplicacion.personas;
-
-public class Mensajero {
+import gestorAplicacion.productos.*;
+import java.util.*;
+public class Mensajero extends Persona {
+	public String transporte;
+	public int contador;
+	public int gan_adicional;
+	public ArrayList <Factura> facturas=new ArrayList <Factura>();
+	public Mensajero(String nombre, String id,String genero,String direccion,int tel_fijo,int celular,String transporte, int contador, int gan_adicional, ArrayList<Factura> facturas) {
+		super(nombre,id,genero,direccion,tel_fijo,celular);
+		this.transporte = transporte;
+		this.contador = contador;
+		this.gan_adicional = gan_adicional;
+		this.facturas = facturas;
+	}
+	
+	
 
 }
