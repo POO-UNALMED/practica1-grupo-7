d746b793b4d7549fe47facaf02d387e7235d5a34
Pai-san <62859618+Pai-san@users.noreply.github.com>
2020-10-04 21:36:34 -0500

    commo2

--- src/inicio/Factura.java
+++ src/inicio/Factura.java
@@ -2,7 +2,7 @@ package inicio;
 
 import java.util.ArrayList;
 public class Factura {
-	public String idMensajero;
+	public Mensajero idMen;
 	public String idFactura;
 	public Usuario user;
 	public double total;
@@ -13,4 +13,16 @@ public class Factura {
 	Supermercado superm;
 	Compra compra;
 	public static ArrayList <DetFactura> Carrito = new ArrayList<DetFactura>();
+    public Factura(Mensajero idM,String idFac,Usuario user,double total,double iva,double subTotal,String banco,String telAtencion,Supermercado superm,Compra compra) {
+    	this.idMen=idM;
+    	this.idFactura=idFac;
+    	this.user=user;
+    	this.total=total;
+    	this.iva=iva;
+    	this.subTotal_detallefac=subTotal;
+    	this.banco=banco;
+    	this.telAtencion=telAtencion;
+    	this.superm=superm;
+    	this.compra=compra;		
+    }
 }
