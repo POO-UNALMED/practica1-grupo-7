3d6fd491641acfe0757ecde5e25933b63d5a5cdc
Edwin Jimenez Gomez <erjimene@unal.edu.co>
2020-10-05 18:10:28 -0500

    prueb5

--- src/gestorAplicacion/productos/Supermercado.java
+++ src/gestorAplicacion/productos/Supermercado.java
@@ -10,5 +10,25 @@ public class Supermercado {
 		this.direccion = direccion;
 		this.tel = tel;
 		this.canasta_sugerencias = canasta_sugerencias;
+	
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public void setTel(int tel) {
+		this.tel = tel;
 	}
 }	
