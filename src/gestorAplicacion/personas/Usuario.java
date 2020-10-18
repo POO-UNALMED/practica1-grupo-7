6a95597a4f647096219fd6221a737255ae256d51
Pai-san <62859618+Pai-san@users.noreply.github.com>
2020-10-18 11:19:55 -0500

    Update Usuario.java

--- src/gestorAplicacion/personas/Usuario.java
+++ src/gestorAplicacion/personas/Usuario.java
@@ -3,30 +3,15 @@ import java.util.ArrayList;
 import gestorAplicacion.productos.*;
 
 public class Usuario extends Persona implements Empleado{
-	private String nomUsuario;
-	private String contrasena;
+
 	public int caja;
-	public int salario_cajero;
+	public static final int salario_cajero=75000;
 	public  ArrayList <Compra> Compras = new ArrayList<Compra>();
 	private static int totalUsuarios=0;
-	Usuario(String nombre,String id,String genero,String direccion,String telFijo,String numCelular,String nomUsuario,String contrasena,int Caja){
+	Usuario(String nombre,String id,String genero,String direccion,String telFijo,String numCelular,int Caja){
 		super(nombre,id,genero,direccion,telFijo,numCelular);
-		this.nomUsuario=nomUsuario;
-		this.contrasena=contrasena;
 		this.caja=Caja;
 		setTotalUsuarios(getTotalUsuarios()+1);
-	}
-	public String getNomUsuario() {
-		return nomUsuario;
-	}
-	public void setNomUsuario(String nomUsuario) {
-		this.nomUsuario = nomUsuario;
-	}
-	public String getContrasena() {
-		return contrasena;
-	}
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
 	}
 	public static int getTotalUsuarios() {
 		return totalUsuarios;
