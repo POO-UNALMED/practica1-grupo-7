5639dc345dd620b62913fb5f559ce927dae0ebea
Pai-san <62859618+Pai-san@users.noreply.github.com>
2020-10-27 17:02:24 -0500

    commit

--- src/gestorAplicacion/productos/DetalleFactura.java
+++ src/gestorAplicacion/productos/DetalleFactura.java
@@ -6,7 +6,6 @@ public class DetalleFactura {
 	private int cantidad;
 	private double subtotal;
 	public DetalleFactura(Factura id_factura, Producto id_producto, int cantidad, double subtotal) {
-		super();
 		this.id_factura = id_factura;
 		this.id_producto = id_producto;
 		this.cantidad = cantidad;
